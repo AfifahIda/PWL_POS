96aa2a8c6415b8def249930661e3416e9166aed9
Afifah Ida Setyo Rini <afifah.ida.a57@gmail.com>
2024-09-23 00:09:38 +0700

    Praktikum 5 - Js 3

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LevelController;
+use App\Http\Controllers\KategoriController;
 
 
 Route::get('/', function () {
@@ -9,6 +10,7 @@ Route::get('/', function () {
 });
 
 Route::get('/level', [LevelController::class, 'index']);
+Route::get('/kategori', [KategoriController::class, 'index']);
 
 
 /*
