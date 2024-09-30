d76ed20130ed848322ee47732729adfa2b26e35b
Afifah Ida Setyo Rini <afifah.ida.a57@gmail.com>
2024-09-30 10:45:09 +0700

    Praktikum 1 dan 2 - Js 5

--- routes/web.php
+++ routes/web.php
@@ -16,14 +16,15 @@ Route::get('/user', [UserController::class, 'index']);
 
 
 Route::get('/user/tambah', [UserController::class, 'tambah']);
-
 Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
-
 Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
-
 Route::get('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
-
 Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
+
+
+use App\Http\Controllers\WelcomeController;
+Route::get('/', [WelcomeController::class, 'index']);
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
