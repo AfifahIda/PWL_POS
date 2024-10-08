711f2aa2615c78d49e0785d74ad5d29f2cb0d6ea
AfifahIda <afifah.ida.a57@gmail.com>
2024-10-08 12:55:38 +0700

    Tugas Praktikum - Js 5

--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
+use App\Models\KategoriModel;  
+use Yajra\DataTables\Facades\DataTables;
+use Illuminate\Database\QueryException;
 
 class KategoriController extends Controller
 {
@@ -23,7 +25,173 @@ class KategoriController extends Controller
         //$row = DB::table('m_kategori')->where('kategori_kode', 'SNK')->delete();
         //return 'Delete data berhasil. Jumlah data yang dihapus: ' . $row.' baris';
     
-        $data = DB::table('m_kategori')->get();
-        return view('kategori', ['data' => $data]);
+        //$data = DB::table('m_kategori')->get();
+        // return view('kategori', ['data' => $data]);
+    
+        $breadcrumb = (object) [
+            'title' => 'Daftar Kategori',
+            'list'  => ['Home', 'Kategori']
+        ];
+
+        $page = (object) [
+            'title' => 'Daftar kategori yang terdaftar dalam sistem'
+        ];
+        $activeMenu = 'kategori'; // set menu yang sedang aktif
+
+        $kategori = KategoriModel::all();
+
+        return view('kategori.index', [
+            'breadcrumb' => $breadcrumb,
+            'page'       => $page,
+            'kategori'      => $kategori,
+            'activeMenu' => $activeMenu
+        ]);
+    }
+
+    public function list(Request $request)
+    {
+        $kategori = KategoriModel::all();
+
+        return DataTables::of($kategori)
+            ->addIndexColumn()
+            ->addColumn('aksi', function ($kategori) {
+                $btn = '<a href="' . url('/kategori/' . htmlspecialchars($kategori->kategori_id)) . '" class="btn btn-info btn-sm">Detail</a> ';
+                $btn .= '<a href="' . url('/kategori/' . htmlspecialchars($kategori->kategori_id) . '/edit') . '" class="btn btn-warning btn-sm">Edit</a>';
+                $btn .= '<form class="d-inline-block" method="POST" action="' . url('/kategori/' . htmlspecialchars($kategori->kategori_id)) . '">
+                            ' . csrf_field() . method_field('DELETE') . '
+                            <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm(\'Apakah Anda yakin menghapus data ini?\')">Hapus</button>
+                        </form>';
+                return $btn;
+            })
+            ->rawColumns(['aksi'])
+            ->make(true);
+    }
+
+    // Menampilkan halaman form tambah user
+    public function create()
+    {
+        $breadcrumb = (object) [
+            'title' => 'Tambah Kategori',
+            'list'  => ['Home', 'Kategori', 'Tambah']
+        ];
+
+        $page = (object) [
+            'title' => 'Tambah kategori baru'
+        ];
+
+        $kategori = KategoriModel::all(); 
+        $activeMenu = 'kategori'; 
+
+        return view('kategori.create', [
+            'breadcrumb' => $breadcrumb,
+            'page'       => $page,
+            'kategori'   => $kategori,
+            'activeMenu' => $activeMenu
+        ]);
+    }
+
+    public function store(Request $request){
+    // Validasi input
+    $request->validate([
+        'kategori_kode' => 'required|string|min:2|max:10|unique:m_kategori,kategori_kode',
+        'kategori_nama' => 'required|string|max:100',
+    ]);
+
+    // Membuat data user baru dengan data yang sudah divalidasi
+    KategoriModel::create([
+        'kategori_kode' => $request->kategori_kode,
+        'kategori_nama' => $request->kategori_nama,
+    ]);
+
+    // Redirect ke halaman user dengan pesan sukses
+    return redirect('/kategori')->with('success', 'Data kategori berhasil disimpan');
+    }
+
+    // menampilkan detail user
+    public function show(string $id)
+{
+    // Ambil data user beserta levelnya berdasarkan ID
+    $kategori = KategoriModel::find($id);
+
+    // Siapkan data untuk breadcrumb (navigasi)
+    $breadcrumb = (object) [
+        'title' => 'Detail Kategori',
+        'list' => ['Home', 'Kategori', 'Detail']
+    ];
+
+    // Siapkan data untuk halaman (page)
+    $page = (Object) [
+        'title' => 'Detail Kategori'
+    ];
+
+    // Set menu yang aktif
+    $activeMenu = 'kategori';
+
+    // Kembalikan view 'user.show' dengan data yang telah disiapkan
+    return view('kategori.show', [
+        'breadcrumb' => $breadcrumb,
+        'page' => $page,
+        'kategori' => $kategori,
+        'activeMenu' => $activeMenu
+    ]);
+}
+
+    // Fungsi untuk menampilkan form edit user
+    public function edit(string $id)
+    {
+        $kategori = KategoriModel::find($id);
+
+        $breadcrumb = (object) [
+            'title' => 'Edit Kategori',
+            'list' => ['Home', 'Kategori', 'Edit']
+        ];
+
+        $page = (Object) [
+            'title' => 'Edit Kategori'
+        ];
+
+        $activeMenu = 'kategori';
+
+        return view('kategori.edit', [
+            'breadcrumb' => $breadcrumb,
+            'page' => $page,
+            'kategori' => $kategori,
+            'activeMenu' => $activeMenu
+        ]);
+    }
+
+    // Fungsi untuk menyimpan perubahan data user
+    public function update(Request $request, string $id)
+    {
+        // Validasi input dari user
+        $request->validate([
+            'kategori_kode' => 'required|string|min:2|max:10|unique:m_kategori,kategori_kode,' . $id . ',kategori_id',
+            'kategori_nama' => 'required|string|max:100'
+        ]);
+
+        KategoriModel::find($id)->update([
+            'kategori_kode' => $request->kategori_kode,
+            'kategori_nama' => $request->kategori_nama
+        ]);
+
+        return redirect('/kategori')->with('success', 'Data kategori berhasil diubah');
+    }
+
+    // Fungsi untuk menghapus data pengguna berdasarkan ID
+    public function destroy(string $id)
+    {
+        $check = KategoriModel::find($id);
+
+        if (!$check) {
+            return redirect('/kategori')->with('error', "Data kategori tidak ditemukan");
+        }
+
+        try {
+            KategoriModel::destroy($id);
+            return redirect('/kategori')->with('success', 'Data kategori berhasil dihapus');
+        } catch (QueryException $e) { 
+
+            return redirect('/kategori')->with('error', 'Data kategori gagal dihapus karena masih terdapat tabel lain yang terkait dengan data ini');
+        }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\LevelController;
-use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\UserController;
 
 
@@ -10,8 +8,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/level', [LevelController::class, 'index']);
-Route::get('/kategori', [KategoriController::class, 'index']);
+//Route::get('/level', [LevelController::class, 'index']);
+//Route::get('/kategori', [KategoriController::class, 'index']);
 Route::get('/user', [UserController::class, 'index']);
 
 
@@ -36,6 +34,65 @@ Route::group(['prefix' => 'user'], function () {
     Route::delete('/{id}', [UserController::class, 'destroy']);  // menghapus data user
 });
 
+
+use App\Http\Controllers\KategoriController;
+Route::get('/', [KategoriController::class, 'index']);
+
+Route::group(['prefix' => 'kategori'], function () {
+    Route::get('/', [KategoriController::class, 'index']);
+    Route::post('/list', [KategoriController::class, 'list']);
+    Route::get('/create', [KategoriController::class, 'create']);
+    Route::post('/', [KategoriController::class, 'store']);
+    Route::get('/{id}', [KategoriController::class, 'show']);
+    Route::get('/{id}/edit', [KategoriController::class, 'edit']);
+    Route::put('/{id}', [KategoriController::class, 'update']);
+    Route::delete('/{id}', [KategoriController::class, 'destroy']);
+});
+
+
+use App\Http\Controllers\BarangController;
+Route::get('/', [BarangController::class, 'index']);
+
+Route::group(['prefix' => 'barang'], function () {
+    Route::get('/', [BarangController::class, 'index']);
+    Route::post('/list', [BarangController::class, 'list']);
+    Route::get('/create', [BarangController::class, 'create']);
+    Route::post('/', [BarangController::class, 'store']);
+    Route::get('/{id}', [BarangController::class, 'show']);
+    Route::get('/{id}/edit', [BarangController::class, 'edit']);
+    Route::put('/{id}', [BarangController::class, 'update']);
+    Route::delete('/{id}', [BarangController::class, 'destroy']);
+});
+
+
+use App\Http\Controllers\SupplierController;
+Route::get('/', [SupplierController::class, 'index']);
+
+Route::group(['prefix' => 'supplier'], function () {
+    Route::get('/', [SupplierController::class, 'index']);
+    Route::post('/list', [SupplierController::class, 'list']);
+    Route::get('/create', [SupplierController::class, 'create']);
+    Route::post('/', [SupplierController::class, 'store']);
+    Route::get('/{id}', [SupplierController::class, 'show']);
+    Route::get('/{id}/edit', [SupplierController::class, 'edit']);
+    Route::put('/{id}', [SupplierController::class, 'update']);
+    Route::delete('/{id}', [SupplierController::class, 'destroy']);
+});
+
+
+use App\Http\Controllers\LevelController;
+Route::get('/', [LevelController::class, 'index']);
+
+Route::group(['prefix' => 'level'], function () {
+    Route::get('/', [LevelController::class, 'index']);
+    Route::post('/list', [LevelController::class, 'list']);
+    Route::get('/create', [LevelController::class, 'create']);
+    Route::post('/', [LevelController::class, 'store']);
+    Route::get('/{id}', [LevelController::class, 'show']);
+    Route::get('/{id}/edit', [LevelController::class, 'edit']);
+    Route::put('/{id}', [LevelController::class, 'update']);
+    Route::delete('/{id}', [LevelController::class, 'destroy']);
+});
 
 /*
 |--------------------------------------------------------------------------
