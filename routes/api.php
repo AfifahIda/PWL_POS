6cf7d3c45d97026d57de74b781cfeb121e1d55be
AfifahIda <afifah.ida.a57@gmail.com>
2024-11-04 08:25:05 +0700

    Praktikum 2 - Js 10

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\RegisterController;
+use App\Http\Controllers\Api\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,7 +15,8 @@ use App\Http\Controllers\Api\RegisterController;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
+Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('registerapi');
+Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('loginapi');
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
